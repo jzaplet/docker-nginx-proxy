27b7fdb1f881b6f0b80fdb23c7047356f48d738b
Jiří Zapletal <jz@strategio.dev>
2023-07-12 10:42:30 -0600

    Added more info

--- www/index.php
+++ www/index.php
@@ -4,22 +4,28 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 \Tracy\Debugger::enable(\Tracy\Debugger::Development);
 
+dump('$_SERVER ------------------------------------->');
+dump($_SERVER['REMOTE_ADDR']);
+dump($_SERVER['HTTP_X_REAL_IP']);
+dump($_SERVER);
+
+dump('Symfony --------------------------------------->');
+
+$symfony = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$symfony::setTrustedProxies(['192.168.0.0/16', '10.0.0.0/8'], -1);
+
+dump($symfony);
+dump($symfony->isFromTrustedProxy());
+dump($symfony->getHost());
+dump($symfony->getHttpHost());
+dump($symfony->getClientIp());
+
+dump('Nette --------------------------------------->');
+
 $nette = (new Nette\Http\RequestFactory);
 $nette->setProxy(['192.168.0.0/16', '10.0.0.0/8']);
 $netteRequest = $nette->fromGlobals();
 
-
-$symfony = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$symfony::setTrustedProxies(['192.168.0.0/16', '10.0.0.0/8'], -1);
-
-dump($symfony->isFromTrustedProxy());
-
-dump($symfony->getClientIp());
-dump($symfony->getClientIps());
-
+dump($netteRequest);
 dump($netteRequest->getRemoteAddress());
 dump($netteRequest->getRemoteHost());
-
-dump($_SERVER['REMOTE_ADDR']);
-dump($_SERVER['HTTP_X_REAL_IP']);
-dump($_SERVER);
