9df1fe7345acb9c8da5397a7f94e6f169ca83b6a
Jiří Zapletal <jz@strategio.dev>
2023-07-12 09:39:38 -0600

    Added Symfony & Nette tests

--- www/index.php
+++ www/index.php
@@ -4,4 +4,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 \Tracy\Debugger::enable(\Tracy\Debugger::Development);
 
-dumpe($_SERVER);
+$nette = (new Nette\Http\RequestFactory)->fromGlobals();
+$symfony = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+dump($symfony->getClientIp());
+dump($nette->getRemoteHost());
+
+dump($_SERVER['REMOTE_ADDR']);
+dump($_SERVER['HTTP_X_REAL_IP']);
