37e988ceccd7e36b9aa4949f377b494432664094
Jiří Zapletal <jz@strategio.dev>
2023-07-12 12:00:32 -0600

    Added real-ip into log-formatter

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\Tracy\Debugger::enable(\Tracy\Debugger::Development);
+//\Tracy\Debugger::enable(\Tracy\Debugger::Development);
 
 $trustedProxies = ['192.168.0.0/16', '10.0.0.0/8'];
 
